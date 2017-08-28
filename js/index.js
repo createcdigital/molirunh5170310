467f1ecf405304dbe4c74953103869b7b6327be8
qiaojun0312 <949042858@qq.com>
2017-08-28 14:26:03 +0800

    update kid year and month

--- js/index.js
+++ js/index.js
@@ -1297,17 +1297,30 @@ app.p5.gotopay = function(user_data, pay_data, use_coupon, coupon_code)
     $.post(app.api.host + '/user/add', user_data, function(data){
         var data = typeof data == "object" ? data : JSON.parse(data);
         console.info(data.rs);
-        if(data.rs=="success"){
+        if(data.rs=="success")
+        {
             app.p1.setidCookie("id",""+user.p1_card_number+"");
-
             if(!use_coupon)
                 app.p5.payment(pay_data);
             else
             {
                 app.p5.useconpontopay(user_data, pay_data, coupon_code);
             }
-        }else {
-               alert(data.rs);
+        }else{
+                   if(data.res.indexOf('已报名')!=-1 && data.res.indexOf('未支付')!=-1)
+                    {
+                        app.p1.setidCookie("id",""+user.p1_card_number+"");
+                        if(!use_coupon)
+                            app.p5.payment(pay_data);
+                        else
+                        {
+                            app.p5.useconpontopay(user_data, pay_data, coupon_code);
+                        }
+                    }else
+                    {
+                        alert(data.rs);
+                    }
+            }
         }
     }, "JSON");
 }

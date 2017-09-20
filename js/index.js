b6f8476000336d300489ada89a0f72fde511e357
qiaojun0312 <949042858@qq.com>
2017-09-20 12:55:14 +0800

    index.js

--- js/index.js
+++ js/index.js
@@ -1234,7 +1234,7 @@ app.p5.bind_touch_event = function(){
 		app.template.swiper.prev();
 	});
 	//确认并支付
-	$(".p5-paybtn").on("click",function(){
+	$(".p5-paybtn").on("touchend",function(){
         alert("开始支付");
     	if($(".p3-group-5").is(":checked") || $(".p3-group-10").is(":checked")){
     		app.p5.singlejudge();
